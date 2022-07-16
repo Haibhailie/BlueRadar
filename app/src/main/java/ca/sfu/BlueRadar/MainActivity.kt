063c41b131644b98eb9d5a268fdc052ede137d02
Shashank Thanalapati <Shashankzv@gmail.com>
2022-07-16 01:43:46 -0700

    Added burger menu + Code cleanup

--- app/src/main/java/ca/sfu/BlueRadar/MainActivity.kt
+++ app/src/main/java/ca/sfu/BlueRadar/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -16,24 +17,46 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var burgerToggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
+    lateinit var burgerToggle: ActionBarDrawerToggle
+    lateinit var burgerMenuLayout: DrawerLayout
+    lateinit var burgerView: NavigationView
+    lateinit var navView: BottomNavigationView
+    lateinit var navController:NavController
+    lateinit var appBarConfiguration:AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val burgerMenuLayout: DrawerLayout = findViewById(R.id.burger_layout)
-        val burgerView: NavigationView = findViewById(R.id.burger_view)
+        setupBurgerMenuContents()
+        setupBurgerMenuNavigation()
+
+    }
+
+    private fun setupBurgerMenuNavigation(){
+        navView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_dashboard, R.id.navigation_devices, R.id.navigation_map
+            ), burgerMenuLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    private fun setupBurgerMenuContents(){
+        burgerMenuLayout= findViewById(R.id.burger_layout)
+        burgerView= findViewById(R.id.burger_view)
 
         burgerToggle = ActionBarDrawerToggle(this, burgerMenuLayout, R.string.open, R.string.close)
         burgerMenuLayout.addDrawerListener(burgerToggle)
         burgerToggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         burgerView.setNavigationItemSelectedListener {
             when (it.itemId) {
@@ -46,20 +69,8 @@ class MainActivity : AppCompatActivity() {
                 R.id.burger_logout ->
                     Toast.makeText(applicationContext, "Logged Out", Toast.LENGTH_SHORT).show()
             }
-
             true
         }
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_dashboard, R.id.navigation_devices, R.id.navigation_map
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
