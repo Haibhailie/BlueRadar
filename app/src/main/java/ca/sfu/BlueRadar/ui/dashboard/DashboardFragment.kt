9888f938d45fcb9464429c05caec3fcedba08ee7
Shashank Thanalapati <Shashankzv@gmail.com>
2022-08-02 20:58:33 -0700

    Merge + Layout

--- app/src/main/java/ca/sfu/BlueRadar/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/ca/sfu/BlueRadar/ui/dashboard/DashboardFragment.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import ca.sfu.BlueRadar.databinding.FragmentDashboardBinding
 import ca.sfu.BlueRadar.ui.devices.DeviceViewModel
 import ca.sfu.BlueRadar.ui.devices.DeviceViewModelFactory
+import ca.sfu.BlueRadar.ui.devices.DevicesFragment
 import ca.sfu.BlueRadar.ui.devices.data.Device
 import ca.sfu.BlueRadar.ui.devices.data.DeviceDatabase
 import ca.sfu.BlueRadar.ui.devices.data.DeviceDatabaseDao
@@ -49,33 +50,37 @@ class DashboardFragment : Fragment() {
         database = DeviceDatabase.getInstance(requireActivity())
         databaseDao = database.deviceDatabaseDao
         viewModelFactory = DeviceViewModelFactory(databaseDao)
-        deviceViewModel = ViewModelProvider(requireActivity(),viewModelFactory)[DeviceViewModel::class.java]
+        deviceViewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory)[DeviceViewModel::class.java]
         deviceViewModel.allEntriesLiveData.observe(viewLifecycleOwner) {
             println("DEVICE: $it\n")
-        if(!deviceViewModel.allEntriesLiveData.value.isNullOrEmpty()){
-            setupRecyclerView()
-            for(i in deviceViewModel.allEntriesLiveData.value!!) {
-                Log.d("check_from_dash", i.toString())
-            }
-        } else {
-            val textView: TextView = binding.dashboardTitle
-            dashboardViewModel.text.observe(viewLifecycleOwner) {
-                textView.text = it
-                textView.setTextColor(Color.GRAY)
+            if (!deviceViewModel.allEntriesLiveData.value.isNullOrEmpty()) {
+                setupRecyclerView()
+                for (i in deviceViewModel.allEntriesLiveData.value!!) {
+                    Log.d("check_from_dash", i.toString())
+                }
+            } else {
+                val textView: TextView = binding.dashboardTitle
+                dashboardViewModel.text.observe(viewLifecycleOwner) {
+                    textView.text = it
+                    textView.setTextColor(Color.GRAY)
+                }
             }
         }
         return root
     }
 
-    private fun setupRecyclerView() {
+    fun setupRecyclerView() {
         recyclerView = binding.devicesRecyclerDashboard
         recyclerView.layoutManager = GridLayoutManager(requireActivity(), 1)
         arrayList = ArrayList()
-        recyclerAdapter = DashboardRecyclerAdapter(requireActivity(), arrayList, deviceViewModel)
+        recyclerAdapter =
+            DashboardRecyclerAdapter(requireActivity(), arrayList, deviceViewModel)
         recyclerView.adapter = recyclerAdapter
         recyclerView.layoutManager = GridLayoutManager(requireActivity(), 1)
         arrayList = ArrayList()
-        recyclerAdapter = DashboardRecyclerAdapter(requireActivity(), arrayList, deviceViewModel)
+        recyclerAdapter =
+            DashboardRecyclerAdapter(requireActivity(), arrayList, deviceViewModel)
         Util.removeDuplicates(arrayList)
         deviceViewModel.activeEntriesLiveData.observe(viewLifecycleOwner) {
             recyclerAdapter.replace(it)
@@ -87,10 +92,12 @@ class DashboardFragment : Fragment() {
             MarginItemDecoration(25)
         )
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
     class MarginItemDecoration(private val spaceSize: Int) : RecyclerView.ItemDecoration() {
         override fun getItemOffsets(
             outRect: Rect, view: View,
